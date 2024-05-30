0f46668a47fdf0130bee0340ba7ed6de076fa54f
Aashish-Dahal <ashishdahal490@gmail.com>
2024-05-30 17:30:43 +0545

    feat: get bulk steps

--- lib/hive_storage.dart
+++ lib/hive_storage.dart
@@ -3,6 +3,7 @@ import 'package:hive/hive.dart';
 import 'package:intl/intl.dart';
 
 class HiveStorage {
+  final String boxKey = "daily_steps";
   HiveStorage._();
   static HiveStorage get instance => HiveStorage._();
   registerStepDataAdapter() async {
@@ -13,25 +14,37 @@ class HiveStorage {
     final date = DateFormat('yyyy-MM-dd').format(stepData.date);
     late Box<StepData> dailyStepCountBox;
 
-    final isOpen = Hive.isBoxOpen("daily_steps");
+    final isOpen = Hive.isBoxOpen(boxKey);
     if (isOpen) {
-      dailyStepCountBox = await Hive.box("daily_steps");
+      dailyStepCountBox = await Hive.box(boxKey);
       await dailyStepCountBox.put(date, stepData);
     } else {
-      dailyStepCountBox = await Hive.openBox("daily_steps");
+      dailyStepCountBox = await Hive.openBox(boxKey);
       return dailyStepCountBox.put(date, stepData);
     }
   }
 
   Future<StepData?> getDailySteps({required String date}) async {
     late Box<StepData> dailyStepCountBox;
-    final isOpen = Hive.isBoxOpen("daily_steps");
+    final isOpen = Hive.isBoxOpen(boxKey);
     if (!isOpen) {
-      dailyStepCountBox = await Hive.box("daily_steps");
+      dailyStepCountBox = await Hive.box(boxKey);
       return dailyStepCountBox.get(date);
     } else {
-      dailyStepCountBox = await Hive.openBox("daily_steps");
+      dailyStepCountBox = await Hive.openBox(boxKey);
       return dailyStepCountBox.get(date);
+    }
+  }
+
+  Future<List<StepData>?> getBulkSteps() async {
+    late Box<StepData> dailyStepCountBox;
+    final isOpen = Hive.isBoxOpen(boxKey);
+    if (!isOpen) {
+      dailyStepCountBox = await Hive.box(boxKey);
+      return dailyStepCountBox.values.toList();
+    } else {
+      dailyStepCountBox = await Hive.openBox(boxKey);
+      return dailyStepCountBox.values.toList();
     }
   }
 }
