3cf11218805d1a15f3a5c2ce4931f9a3c3f4048c
Aashish-Dahal <ashishdahal490@gmail.com>
2024-06-11 16:16:15 +0545

    fix

--- android/src/main/kotlin/com/example/daily_pedometer2/DailyStepCountHandler.kt
+++ android/src/main/kotlin/com/example/daily_pedometer2/DailyStepCountHandler.kt
@@ -79,14 +79,9 @@ class DailyStepCountHandler() : EventChannel.StreamHandler {
     private fun isDifferentDay(savedDate: Long): Boolean {
         val savedCalendar = Calendar.getInstance().apply { timeInMillis = savedDate }
         val currentCalendar = Calendar.getInstance()
-        val threeOhFivePM = Calendar.getInstance().apply {
-            set(Calendar.HOUR_OF_DAY, 16)
-            set(Calendar.MINUTE, 10)
-            set(Calendar.SECOND, 0)
-            set(Calendar.MILLISECOND, 0)
-        }
+
         return savedCalendar.get(Calendar.DAY_OF_YEAR) != currentCalendar.get(Calendar.DAY_OF_YEAR) ||
-                savedCalendar.get(Calendar.YEAR) != currentCalendar.get(Calendar.YEAR) || currentCalendar.after(threeOhFivePM)
+                savedCalendar.get(Calendar.YEAR) != currentCalendar.get(Calendar.YEAR) || (currentCalendar.get(Calendar.HOUR_OF_DAY) == 16 && currentCalendar.get(Calendar.MINUTE) == 20)
     }
     private fun resetStepCount() {
  // Reset step count at the start of a new day
