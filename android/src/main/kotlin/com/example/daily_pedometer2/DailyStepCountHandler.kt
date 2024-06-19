89f56f7395f4bc547c56b2a8de9b418f0a2841e4
Bivek10 <bibekkarki662@gmail.com>
2024-06-19 18:28:54 +0545

    fix: testing

--- android/src/main/kotlin/com/example/daily_pedometer2/DailyStepCountHandler.kt
+++ android/src/main/kotlin/com/example/daily_pedometer2/DailyStepCountHandler.kt
@@ -51,6 +51,7 @@ class DailyStepCountHandler() : EventChannel.StreamHandler {
             override fun onSensorChanged(event: SensorEvent?) {
                 if (event?.sensor?.type == Sensor.TYPE_STEP_COUNTER) {
                     val currentStepCount = event.values[0].toInt();
+                    Log.d("Different----Day::: ${isDifferentDay(sharedPrefs.getLong("lastSavedDate", 0L)}")
                     if (isDifferentDay(sharedPrefs.getLong("lastSavedDate", 0L))) {
                         Log.d("isDifferentDay", "true")
                         resetStepCount()
@@ -89,7 +90,7 @@ class DailyStepCountHandler() : EventChannel.StreamHandler {
         val currentCalendar = Calendar.getInstance()
 
         return savedCalendar.get(Calendar.DAY_OF_YEAR) != currentCalendar.get(Calendar.DAY_OF_YEAR) ||
-                savedCalendar.get(Calendar.YEAR) != currentCalendar.get(Calendar.YEAR) || (currentCalendar.get(Calendar.HOUR_OF_DAY) == 18 && currentCalendar.get(Calendar.MINUTE) == 10)
+                savedCalendar.get(Calendar.YEAR) != currentCalendar.get(Calendar.YEAR) || (currentCalendar.get(Calendar.HOUR_OF_DAY) == 18 && currentCalendar.get(Calendar.MINUTE) == 38)
     //    // Get the current hour and minute in 12-hour format with AM/PM
     //     int hour = currentCalendar.get(Calendar.HOUR);
     //     int minute = currentCalendar.get(Calendar.MINUTE);
