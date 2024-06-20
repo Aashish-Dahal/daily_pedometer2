7469eaef5f44121f90e37c9200a9dde6001b3a6b
Aashish-Dahal <ashishdahal490@gmail.com>
2024-06-20 12:42:21 +0545

    fix: reset stream

--- android/src/main/kotlin/com/example/daily_pedometer2/DailyPedometer2Plugin.kt
+++ android/src/main/kotlin/com/example/daily_pedometer2/DailyPedometer2Plugin.kt
@@ -9,8 +9,6 @@ import io.flutter.plugin.common.EventChannel
 import io.flutter.plugin.common.MethodChannel
 import android.util.Log
 
-
-
 /** DailyPedometer2Plugin */
 class DailyPedometer2Plugin : FlutterPlugin {
     private lateinit var stepDetectionChannel: EventChannel
@@ -18,19 +16,25 @@ class DailyPedometer2Plugin : FlutterPlugin {
     private lateinit var dailyStepCountChannel: EventChannel
     private lateinit var isDifferentDayChannel: MethodChannel
 
-    override fun onAttachedToEngine(@NonNull flutterPluginBinding: FlutterPlugin.FlutterPluginBinding) {
-        /// Create channels
+    private lateinit var stepDetectionHandler: SensorStreamHandler
+    private lateinit var stepCountHandler: SensorStreamHandler
+    private lateinit var dailyStepCountHandler: DailyStepCountHandler
 
+    override fun onAttachedToEngine(@NonNull flutterPluginBinding: FlutterPlugin.FlutterPluginBinding) {
+        initializeChannelsAndHandlers(flutterPluginBinding)
+    }
+
+    private fun initializeChannelsAndHandlers(flutterPluginBinding: FlutterPlugin.FlutterPluginBinding) {
+        /// Create channels
         stepDetectionChannel = EventChannel(flutterPluginBinding.binaryMessenger, "step_detection")
         stepCountChannel = EventChannel(flutterPluginBinding.binaryMessenger, "step_count")
         dailyStepCountChannel = EventChannel(flutterPluginBinding.binaryMessenger, "daily_step_count")
         isDifferentDayChannel = MethodChannel(flutterPluginBinding.binaryMessenger, "reset_step_count")
-      
         
         /// Create handlers
-        val stepDetectionHandler = SensorStreamHandler(flutterPluginBinding, Sensor.TYPE_STEP_DETECTOR)
-        val stepCountHandler = SensorStreamHandler(flutterPluginBinding, Sensor.TYPE_STEP_COUNTER)
-        val dailyStepCountHandler = DailyStepCountHandler(flutterPluginBinding)
+        stepDetectionHandler = SensorStreamHandler(flutterPluginBinding, Sensor.TYPE_STEP_DETECTOR)
+        stepCountHandler = SensorStreamHandler(flutterPluginBinding, Sensor.TYPE_STEP_COUNTER)
+        dailyStepCountHandler = DailyStepCountHandler(flutterPluginBinding)
 
         isDifferentDayChannel.setMethodCallHandler { call, result ->
             Log.d("Method channel", "isDifferentDay")
@@ -40,19 +44,23 @@ class DailyPedometer2Plugin : FlutterPlugin {
             } 
         }
 
-        private fun reinitializeStreams() {
-            /// Reset the handlers
-            stepDetectionChannel.setStreamHandler(null)
-            stepCountChannel.setStreamHandler(null)
-            dailyStepCountChannel.setStreamHandler(null)
-            
-            /// Reattach the handlers to the channels
-            stepDetectionChannel.setStreamHandler(stepDetectionHandler)
-            stepCountChannel.setStreamHandler(stepCountHandler)
-            dailyStepCountChannel.setStreamHandler(dailyStepCountHandler)
-        }
-
         /// Set handlers
+        stepDetectionChannel.setStreamHandler(stepDetectionHandler)
+        stepCountChannel.setStreamHandler(stepCountHandler)
+        dailyStepCountChannel.setStreamHandler(dailyStepCountHandler)
+    }
+
+    private fun reinitializeStreams() {
+        Log.d("Method channel", "Reset the handlers")
+
+        /// Reset the handlers
+        stepDetectionChannel.setStreamHandler(null)
+        stepCountChannel.setStreamHandler(null)
+        dailyStepCountChannel.setStreamHandler(null)
+        
+        /// Reattach the handlers to the channels
+        Log.d("Method channel", " Reattach the handlers to the channels")
+
         stepDetectionChannel.setStreamHandler(stepDetectionHandler)
         stepCountChannel.setStreamHandler(stepCountHandler)
         dailyStepCountChannel.setStreamHandler(dailyStepCountHandler)
@@ -63,5 +71,4 @@ class DailyPedometer2Plugin : FlutterPlugin {
         stepCountChannel.setStreamHandler(null)
         dailyStepCountChannel.setStreamHandler(null)
     }
-
 }
